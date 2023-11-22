56c937ce5005ac34a5e8a547df6ebdc0224ad808
Oscar Veldman <oveldman@gmail.com>
2023-11-22 22:47:27 +0100

    Test publish

--- MadWorld/Deployments/Scripts/local-build-and-deploy.sh
+++ MadWorld/Deployments/Scripts/local-build-and-deploy.sh
@@ -17,6 +17,6 @@ build_and_load_image "MadWorld.Backend.API" "api"
 sudo docker image prune -f
 
 minikube kubectl -- apply -f Deployments/Kubernetes/Environment/Config.yaml
-minikube kubectl -- apply -f Deployments/Kubernetes/Environment/Ingress-Deployment-Local.yaml
 minikube kubectl -- apply -f Deployments/Kubernetes/Internal
+minikube kubectl -- apply -f Deployments/Kubernetes/Environment/Ingress-Deployment-Local.yaml
 #minikube kubectl -- apply -f Deployments/Kubernetes/External
