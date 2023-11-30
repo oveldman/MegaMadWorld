d62c3d3a8831dc7685a384b5fe8ee51770238aa9
Oscar Veldman <oveldman@gmail.com>
2023-11-30 22:20:01 +0100

    Refactor

--- MadWorld/MadWorld.Backend.Identity/Endpoints/IdentityEndpoints.cs
+++ MadWorld/MadWorld.Backend.Identity/Endpoints/IdentityEndpoints.cs
@@ -1,7 +1,6 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
