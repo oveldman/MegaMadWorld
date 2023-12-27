cbd9f82749ff38bca0947b8715408068f5fe40f7
Oscar Veldman <oveldman@gmail.com>
2023-12-27 02:40:54 +0100

    Refactor State Provider

--- MadWorld/MadWorld.Shared.Blazor/Authentications/MyAuthenticationStateProvider.cs
+++ MadWorld/MadWorld.Shared.Blazor/Authentications/MyAuthenticationStateProvider.cs
@@ -19,33 +19,58 @@ public class MyAuthenticationStateProvider : AuthenticationStateProvider
     
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-        var identity = new ClaimsIdentity();
-
+        var jwtResponse = await TryGetJwtResponse();
+        var identity = CreateIdentity(jwtResponse);
+        return CreateState(identity);
+    }
+    
+    private async Task<JwtLoginResponse?> TryGetJwtResponse()
+    {
         JwtLoginResponse? jwtResponse = null;
         if (await _localStorageService.ContainKeyAsync(LocalStorageKeys.JwtToken))
         {
             jwtResponse = await _localStorageService.GetItemAsync<JwtLoginResponse>(LocalStorageKeys.JwtToken);            
         }
 
+        return jwtResponse;
+    }
+
+    private ClaimsIdentity CreateIdentity(JwtLoginResponse? jwtResponse)
+    {
+        var identity = new ClaimsIdentity();
+        
         if (jwtResponse is not null)
         {
-            try
-            {
-                identity = RetrieveUserFromJwt(jwtResponse.Jwt);
-                _accessTokenWriter.SetAccessToken(jwtResponse.Jwt, jwtResponse.Expires);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-                identity = new ClaimsIdentity();
-                _accessTokenWriter.RemoveToken();
-            }
+            identity = TryGetIdentityFromToken(jwtResponse);
         }
         else
         {
             _accessTokenWriter.RemoveToken();
         }
-        
+
+        return identity;
+    }
+
+    private ClaimsIdentity TryGetIdentityFromToken(JwtLoginResponse jwtResponse)
+    {
+        ClaimsIdentity identity;
+        try
+        {
+            identity = RetrieveUserFromJwt(jwtResponse.Jwt);
+            _accessTokenWriter.SetAccessToken(jwtResponse.Jwt, jwtResponse.Expires);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            identity = new ClaimsIdentity();
+            _accessTokenWriter.RemoveToken();
+        }
+
+        return identity;
+    }
+    
+    private AuthenticationState CreateState(ClaimsIdentity identity)
+    {
         var user = new ClaimsPrincipal(identity);
         var state = new AuthenticationState(user);
         
