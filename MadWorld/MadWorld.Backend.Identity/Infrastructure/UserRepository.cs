740e0f953ea5b38d447a0caf6f73303ba9b6b086
Oscar Veldman <oveldman@gmail.com>
2024-01-04 20:23:08 +0100

    Add EF performance

--- MadWorld/MadWorld.Backend.Identity/Infrastructure/UserRepository.cs
+++ MadWorld/MadWorld.Backend.Identity/Infrastructure/UserRepository.cs
@@ -1,4 +1,5 @@
 using MadWorld.Backend.Identity.Domain.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace MadWorld.Backend.Identity.Infrastructure;
 
@@ -13,17 +14,26 @@ public class UserRepository : IUserRepository
     
     public List<IdentityUserExtended> GetUsers()
     {
-        return _context.Users.ToList();
+        return _context
+                .Users
+                .AsNoTracking()
+                .ToList();
     }
     
     public Task AddRefreshToken(RefreshToken token)
     {
-        _context.RefreshTokens.Add(token);
+        _context
+            .RefreshTokens
+            .Add(token);
+        
         return _context.SaveChangesAsync();
     }
     
     public RefreshToken? GetRefreshToken(string token)
     {
-        return _context.RefreshTokens.FirstOrDefault(t => t.Token == token);
+        return _context
+                .RefreshTokens
+                .AsNoTracking()
+                .FirstOrDefault(t => t.Token == token);
     }
 }
