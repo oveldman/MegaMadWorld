cd176917a0a800546fa1d9495b019ddace62770b
Oscar Veldman <oveldman@gmail.com>
2024-01-29 22:00:52 +0100

    Add test block functie

--- MadWorld/MadWorld.Backend.IntegrationTests/Identity/PatchUserEndpointTests.cs
+++ MadWorld/MadWorld.Backend.IntegrationTests/Identity/PatchUserEndpointTests.cs
@@ -7,7 +7,6 @@ using MadWorld.Backend.Identity.Infrastructure;
 using MadWorld.Backend.IntegrationTests.Common;
 using MadWorld.Shared.Infrastructure.Settings;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
 
@@ -71,6 +70,57 @@ public class PatchUserEndpointTests : IdentityTestBase
             roles.Count.ShouldBe(2);
             roles.ShouldContain(Roles.IdentityAdministrator);
             roles.ShouldContain(Roles.IdentityShipSimulator);
+        }
+    }
+
+    [Fact]
+    public async Task Execute_WhenRequestBlockIsTrue_ShouldSetUserLockedInDatabase()
+    {
+        // Arrange
+        const string email = "test2@test.nl";
+        var userId = Guid.NewGuid();
+        
+        using (var scope = Factory.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+
+            var user = new IdentityUserExtended()
+            {
+                Id = userId.ToString(),
+                Email = email,
+                UserName = email,
+                EmailConfirmed = true,
+                NormalizedEmail = email,
+                NormalizedUserName = email
+            };
+            
+            await context.Users.AddAsync(user);
+            
+            await context.SaveChangesAsync();
+        }
+
+        var client = Factory.CreateClient();
+        
+        // Act
+        var request = new PatchUserRequest()
+        {
+            Id = userId.ToString(),
+            IsBlocked = true,
+            Roles = new []{ Roles.IdentityAdministrator, Roles.IdentityShipSimulator }
+        };
+        
+        var response = await client.PatchAsJsonAsync("/UserManager/User", request);
+        
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        
+        using (var scope = Factory.Services.CreateScope())
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUserExtended>>();
+            var user = await userManager.FindByIdAsync(userId.ToString());
+            user!.Email.ShouldBe(email);
+            user.LockoutEnabled.ShouldBe(true);
+            user.LockoutEnd.ShouldBe(DateTimeOffset.MaxValue);
         }
     }
 }
