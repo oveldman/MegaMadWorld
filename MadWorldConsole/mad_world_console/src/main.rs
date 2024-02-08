2832fca77d4c6e22238f3ccf9bb4131f1f26dade
oveldman <oveldman@gmail.com>
2024-02-08 22:24:39 +0100

    Start with automatic scaling

--- MadWorldConsole/mad_world_console/src/main.rs
+++ MadWorldConsole/mad_world_console/src/main.rs
@@ -1,5 +1,7 @@
 mod windows;
 
+use std::env;
+
 use crate::windows::os_api;
 use crate::windows::screen_api;
 
@@ -9,5 +11,18 @@ fn main() {
         return;
     }
 
-    screen_api::scale();
+    let args: Vec<String> = env::args().collect();
+    let mut scale_up: bool = false;
+    let mut scale_total_options: i32 = 0;
+
+    if (args.len() > 1){
+        scale_total_options = args[1].parse::<i32>().unwrap();
+    }
+
+    if (args.len() > 2){
+        scale_up = args[2] == "up";
+    }
+
+
+    screen_api::scale(scale_up, scale_total_options);
 }
--- MadWorldConsole/mad_world_console/src/windows/screen_api.rs
+++ MadWorldConsole/mad_world_console/src/windows/screen_api.rs
@@ -1,3 +1,64 @@
-pub fn scale() {
-    println!("Set Scale on max");
+extern crate enigo;
+
+use enigo::*;
+use std::{thread, time};
+
+pub fn scale(scale_up: bool, scale_total_options: i32) {
+    let mut enigo = Enigo::new();
+
+    open_display_settings(&mut enigo);
+    open_select_settings_box(&mut enigo);
+    set_scale_settings(&mut enigo, scale_up, scale_total_options);
+    close_settings(&mut enigo);
+}
+
+fn open_display_settings(keyboard: &mut Enigo) {
+    press_key(keyboard, Key::LWin);
+    thread::sleep(time::Duration::from_millis(200));
+    type_word(keyboard, String::from("display"));
+    thread::sleep(time::Duration::from_millis(200));
+    press_key(keyboard, Key::Return);
+    thread::sleep(time::Duration::from_millis(1000));
+}
+
+fn open_select_settings_box(keyboard: &mut Enigo){
+    keyboard.key_down(Key::LShift);
+    press_key(keyboard, Key::Tab);
+    press_key(keyboard, Key::Tab);
+    keyboard.key_up(Key::LShift);
+    thread::sleep(time::Duration::from_millis(200));
+    press_key(keyboard, Key::Return);
+}
+
+fn set_scale_settings(keyboard: &mut Enigo, scale_up: bool, scale_total_options: i32){
+    let move_key: Key = if scale_up { Key::DownArrow } else { Key::UpArrow };
+
+    thread::sleep(time::Duration::from_millis(100));
+
+    let mut i = 0;
+    while (i < scale_total_options)
+    {
+        press_key(keyboard, move_key);
+        i = i + 1;
+    }
+
+    press_key(keyboard, Key::Return);
+    thread::sleep(time::Duration::from_millis(100));
+}
+
+fn close_settings(keyboard: &mut Enigo){
+    keyboard.key_down(Key::Alt);
+    press_key(keyboard, Key::F4);
+    keyboard.key_up(Key::Alt);
+}
+
+fn type_word(keyboard: &mut Enigo, word: String) {
+    for char in word.chars() {
+        press_key(keyboard, Key::Layout(char))
+    }
+}
+
+fn press_key(keyboard: &mut Enigo, key: Key) {
+    keyboard.key_down(key);
+    keyboard.key_up(key);
 }
